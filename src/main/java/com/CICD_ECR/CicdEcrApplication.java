2ddeeba9cdbf5ce3ae0aad2d42eda25abaa3fb79
Mohit50cyber <mohitsingh30031996@gmail.com>
2025-09-21 00:30:17 +0530

    first commit

---
+++ src/main/java/com/CICD_ECR/CicdEcrApplication.java
@@ -0,0 +1,13 @@
+package com.CICD_ECR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdEcrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdEcrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CICD_ECR/CicdEcrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CICD_ECR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdEcrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
